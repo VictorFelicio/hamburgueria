0f75ba49a68bbc02db57e7c7156f0dc9acac7620
Felicio <victor_felicio@hotmail.com>
2024-10-19 16:29:15 -0300

    create Burger Icon

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,9 +1,24 @@
 import { Container } from './style';
-
+import BurgerIcon from './../../assets/burger.svg';
+import MenuIcon from './../../assets/menu.svg';
 export function Sidebar() {
     return (
         <Container>
-            <h1>Sidebar</h1>
+            <button type="button">
+                <MenuIcon />
+            </button>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#" className="active">
+                            <BurgerIcon />
+                            <span>Hambúrgueres</span>
+                        </a>
+                    </li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
         </Container>
     );
 }
--- src/components/Sidebar/style.ts
+++ src/components/Sidebar/style.ts
@@ -2,10 +2,80 @@ import styled from 'styled-components';
 
 export const Container = styled.aside`
     background-color: ${(props) => props.theme.colors.red};
-    width: 7.75rem;
+
     padding: 2rem 0;
     overflow: hidden;
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    button {
+        background: none;
+        width: 100%;
+        border: none;
+    }
+
+    nav {
+        flex: 1;
+        widows: 100%;
+        height: 100%;
+
+        ul {
+            display: flex;
+            height: 100%;
+            flex-direction: column;
+            justify-content: center;
+            gap: 1.5rem;
+        }
+
+        li {
+            a {
+                display: flex;
+                align-items: center;
+                gap: 2rem;
+                width: fit-content;
+                position: relative;
+                padding-left: 1.875rem;
+                padding-right: 1.875rem;
+
+                svg {
+                    fill: ${(props) => props.theme.colors.white};
+                    width: 4rem;
+                    height: 4rem;
+                    transition: fill 0.3s;
+                }
+
+                span {
+                    font-size: 1rem;
+                    transition: color 0.3s;
+                }
+
+                &.active {
+                    &::after {
+                        content: '';
+                        position: absolute;
+                        left: 0;
+                        top: 50%;
+                        bottom: 0;
+                        transform: translateY(-50%);
+
+                        background-color: ${(props) =>
+                            props.theme.colors.yellow};
+
+                        width: 5px;
+                        height: calc(100%+10px);
+                        border-radius: 0 5px 5px 0;
+                    }
+
+                    svg {
+                        fill: ${(props) => props.theme.colors.yellow};
+                    }
+
+                    span {
+                        color: ${(props) => props.theme.colors.yellow};
+                    }
+                }
+            }
+        }
+    }
 `;
