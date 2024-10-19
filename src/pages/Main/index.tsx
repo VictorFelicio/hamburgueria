28809f05aafa27d75c3e6b70dd68be64378347e7
Felicio <victor_felicio@hotmail.com>
2024-10-18 22:23:02 -0300

    initial commit

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,10 @@
+import { Sidebar } from '../../components/Sidebar';
+import { Container } from './style';
+
+export default function Main() {
+    return (
+        <Container>
+            <Sidebar />
+        </Container>
+    );
+}
