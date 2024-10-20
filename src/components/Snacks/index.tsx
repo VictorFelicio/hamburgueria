911bcce1422371e4444ee39f7ef9a11cf5e6e1db
Felicio <victor_felicio@hotmail.com>
2024-10-20 18:48:02 -0300

    create snack section to menu

---
+++ src/components/Snacks/index.tsx
@@ -0,0 +1,37 @@
+import { FiPlus } from 'react-icons/fi';
+import { Container } from './style';
+
+export interface SnacksProps {
+    snacks: Itens[];
+}
+
+interface Itens {
+    id: number;
+    snack: string;
+    name: string;
+    description: string;
+    price: number;
+    image: string;
+}
+
+export function Snacks(props: SnacksProps) {
+    return (
+        <Container>
+            {props.snacks.map((snack) => {
+                return (
+                    <div key={snack.id} className="snack">
+                        <h2>{snack.name}</h2>
+                        <img src={snack.image} alt={snack.description} />
+                        <p>{snack.description}</p>
+                        <div>
+                            <strong>{snack.price}</strong>
+                            <button type="button">
+                                <FiPlus />
+                            </button>
+                        </div>
+                    </div>
+                );
+            })}
+        </Container>
+    );
+}
--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -1,31 +1,14 @@
 import { Head } from '../../../components/Head';
+import { menu } from '../../../data/menu';
+import { Snacks } from '../../../components/Snacks';
 import { SnackTitle } from '../../../components/SnackTitle';
 
 export function Burgers() {
-    const data = [
-        {
-            id: 1,
-            snack: 'burger',
-            name: 'Mega',
-            description:
-                'O artesanal tamanho familia recheado com tres carnes, queijo e bacon.',
-            price: 25.5,
-            image: 'https://i.imgur.com/upjIUnG.jpg',
-        },
-        {
-            id: 2,
-            snack: 'burger',
-            name: 'Extra Bacon',
-            description:
-                'O artesanal familia recheado com carne, queijo e extra bacon.',
-            price: 23.5,
-            image: 'https://i.imgur.com/B4J04AJ.jpg',
-        },
-    ];
     return (
         <>
             <Head title="Hamburgueres" />
             <SnackTitle>hambúrgueres</SnackTitle>
+            <Snacks snacks={menu.burges} />
         </>
     );
 }
