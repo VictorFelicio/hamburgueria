30730ec3c8b45836375b96e4110d450217d51fb2
Felicio <victor_felicio@hotmail.com>
2024-10-20 17:40:16 -0300

    create mock of data burgers

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -2,6 +2,26 @@ import { Head } from '../../../components/Head';
 import { SnackTitle } from '../../../components/SnackTitle';
 
 export function Burgers() {
+    const data = [
+        {
+            id: 1,
+            snack: 'burger',
+            name: 'Mega',
+            description:
+                'O artesanal tamanho familia recheado com tres carnes, queijo e bacon.',
+            price: 25.5,
+            image: 'https://i.imgur.com/upjIUnG.jpg',
+        },
+        {
+            id: 2,
+            snack: 'burger',
+            name: 'Extra Bacon',
+            description:
+                'O artesanal familia recheado com carne, queijo e extra bacon.',
+            price: 23.5,
+            image: 'https://i.imgur.com/B4J04AJ.jpg',
+        },
+    ];
     return (
         <>
             <Head title="Hamburgueres" />
