81f4f65a2be9f93b983aa2282c43d424265cc5ab
Felicio <victor_felicio@hotmail.com>
2024-10-20 18:59:52 -0300

    create utiul to format currency coin

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -1,5 +1,6 @@
 import { FiPlus } from 'react-icons/fi';
 import { Container } from './style';
+import { coinFormat } from '../../utils/coinFormat';
 
 export interface SnacksProps {
     snacks: Itens[];
@@ -24,7 +25,7 @@ export function Snacks(props: SnacksProps) {
                         <img src={snack.image} alt={snack.description} />
                         <p>{snack.description}</p>
                         <div>
-                            <strong>{snack.price}</strong>
+                            <strong>{coinFormat(snack.price)}</strong>
                             <button type="button">
                                 <FiPlus />
                             </button>
